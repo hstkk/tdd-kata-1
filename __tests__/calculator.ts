930f49555f18c9313280e8cde2110e0aca3b5844
Sami Hostikka <hostikka@gmail.com>
2017-11-20 20:14:21 +0200

    It should return 6 for “1\n2,3”

--- __tests__/calculator.ts
+++ __tests__/calculator.ts
@@ -16,4 +16,8 @@ describe('Calculator', () => {
     it('should handle unknown amount of numbers', () => {
         expect(add('1,2,3,4,5')).toBe(15);
     });
+
+    it('should return 6 for “1\n2,3”', () => {
+        expect(add('1\n2,3')).toBe(6);
+    });
 });
