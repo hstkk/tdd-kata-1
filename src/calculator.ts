b31bc470f3fd29ec0d1a776d8276a9cf0fb9477e
Sami Hostikka <hostikka@gmail.com>
2017-11-20 22:02:52 +0200

    Refactor

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,11 +1,8 @@
 import { Error } from 'tslint/lib/error';
 export function add(str: string): number {
     const numbers: ReadonlyArray<number> = split(str).map(numberify);
-    const negatives: ReadonlyArray<number> = numbers.filter(i => i < 0);
-
-    if (negatives.length > 0) {
-        throw new RangeError(`negatives not allowed ${negatives.join(', ')}`);
-    }
+    // tslint:disable-next-line:no-expression-statement
+    validate(numbers);
 
     return numbers.reduce(sum);
 }
@@ -24,4 +21,12 @@ function split(str: string): ReadonlyArray<string> {
 
 function sum(a: number, b: number): number {
     return a + b;
+}
+
+function validate(numbers: ReadonlyArray<number>): void {
+    const negatives: ReadonlyArray<number> = numbers.filter(i => i < 0);
+
+    if (negatives.length > 0) {
+        throw new RangeError(`negatives not allowed ${negatives.join(', ')}`);
+    }
 }
