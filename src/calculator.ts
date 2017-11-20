a5e3b4c4b5a9d00aa676d31cfc9e2090006c7ed8
Sami Hostikka <hostikka@gmail.com>
2017-11-20 21:22:51 +0200

    Support custom delimeters

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,8 +1,12 @@
-const SEPARATOR: RegExp = /,|\n/;
+const DEFAULT_SEPARATOR: RegExp = /,|\n/;
 
 export function add(str: string): number {
-    return str
-        .split(SEPARATOR)
+    const matches = str.match(/\/\/([^']+)\n([^']+)/);
+    const separator = matches && matches[1] || DEFAULT_SEPARATOR;
+    const numbers = matches && matches[2] || str;
+
+    return numbers
+        .split(separator)
         .map(numberify)
         .reduce(sum);
 }
