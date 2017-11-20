18b1c3528359f5864c2799354f97feb35e08efb8
Sami Hostikka <hostikka@gmail.com>
2017-11-20 19:52:57 +0200

    it should return 1 for "1"

--- __tests__/calculator.ts
+++ __tests__/calculator.ts
@@ -4,4 +4,8 @@ describe('Calculator', () => {
     it('should return 0 for an empty string', () => {
         expect(add('')).toBe(0);
     });
+
+    it('should return 1 for "1"', () => {
+        expect(add('1')).toBe(1);
+    });
 });
