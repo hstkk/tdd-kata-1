2f8591d2325000c55dba98bf0aabaceb57fdf0c3
Sami Hostikka <hostikka@gmail.com>
2017-11-20 20:26:31 +0200

    Refactor

--- src/calculator.ts
+++ src/calculator.ts
@@ -3,6 +3,14 @@ const SEPARATOR: RegExp = /,|\n/;
 export function add(str: string): number {
     return str
         .split(SEPARATOR)
-        .map(s => +s)
-        .reduce((sum, int) => sum + int);
+        .map(numberify)
+        .reduce(sum);
+}
+
+function numberify(str: string): number {
+    return +str;
+}
+
+function sum(a: number, b: number): number {
+    return a + b;
 }
