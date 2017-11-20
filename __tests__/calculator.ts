ee3588147644a8ec6e587f778a945264d3f4a95f
Sami Hostikka <hostikka@gmail.com>
2017-11-20 20:00:52 +0200

    It should return 3 for "1,2"

--- __tests__/calculator.ts
+++ __tests__/calculator.ts
@@ -8,4 +8,8 @@ describe('Calculator', () => {
     it('should return 1 for "1"', () => {
         expect(add('1')).toBe(1);
     });
+
+    it('should return 3 for "1,2"', () => {
+        expect(add('1,2')).toBe(3);
+    });
 });
